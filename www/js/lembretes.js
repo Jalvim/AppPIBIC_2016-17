34cc41ba370f9fb840e009cbca6d2df9e9a71a3a
Gabriel <gabrielpmp@gmail.com>
2016-12-08 12:12:15 -0200

    Adicionada a funcionalidades de remover lembretes criados por meio de um botão.

--- www/js/lembretes.js
+++ www/js/lembretes.js
@@ -1,29 +1,14 @@
 $('.modal-trigger').leanModal();
 
-$('#confirmar').on('click', function() {
-    var texto = "";
-
-    $('.badge').parent().each(function(){
-        texto += this.firstChild.textContent + ': ';
-        texto += this.lastChild.textContent + ', ';
-    });
-
-    $('#resumo').empty().text(texto);
-});
-
-$('.acao-limpar').on('click', function() {
-    $('#numero-mesa').val('');
-    $('.badge').remove();
-});
-
+// Função para criação de novos lembretes via popup modal
 function newElement() {
-  var li = document.createElement("li");
-  var li_href = li.setAttribute('href', '#confirmarRemov');
-  var li_class = li.setAttribute('class', 'collection-item modal-trigger');
-  var inputValue = document.getElementById("lembrete").value;
+  var li = document.createElement("li");    // Cria item da lista
+  var li_class = li.setAttribute('class', 'collection-item');    // Seta a classe dos items
+  var inputValue = document.getElementById("lembrete").value;    // Seta o texto colocado pelo usuário
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
 
+  // Verifica se algo foi escrito
   if (inputValue === '') {
     alert("Você precisa escrever algo!");
   } else {
@@ -31,17 +16,36 @@ function newElement() {
   }
   document.getElementById("lembrete").value = "";
 
-}
+  // Cria o botão de fechar com a classe "closebtn" alinhado a direita
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "closebtn right";
+  span.appendChild(txt);
+  li.appendChild(span);
 
+  // Cria a funcionalidade do botão fechar o lembrete
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+
+};
+
+// Limpa o formulário do modal ao clicar "Cancelar"
 function clearText() {
 
   document.getElementById("lembrete").value = "";
 
 }
 
-function removeElement() {
-
-  var elem = document.getElementById('id');
-  elem.parentNode.removeChild(elem);
-
+// Clica no botão fechar para esconder o lembrete
+var close = document.getElementsByClassName("closebtn");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
