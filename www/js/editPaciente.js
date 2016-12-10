7b6a7a8b29cd6fc7ba091c8317b215b2627b6fe4
Joao Alvim <j.rabello.alvim@outlook.com>
2016-12-10 21:13:39 -0200

    Alterações e incremento de funcionalidade

---
+++ www/js/editPaciente.js
@@ -0,0 +1,16 @@
+$('.modal-trigger').leanModal();
+
+// Funções que salvam as alterações feitas na edição do perfil
+function nome() {};
+
+function idade() {};
+
+function prontuario() {};
+
+function email() {};
+
+function internacao() {};
+
+function add() {};
+
+function del() {};
