32cecb29950af688b9f91348acd3b67023e7edcd
Jalvim <j.rabello.alvim@outlook.com>
2016-12-26 13:07:02 -0200

    Implementação da classe DispPaciente, onde se apresentam os métodos de Display de informações.

--- www/js/dispPaciente.js
+++ www/js/dispPaciente.js
@@ -1,7 +1,51 @@
-//Funcões para fazer o display das infos configuradas na lista de pacientes.
-function dispNome() {
-  var nome = jQuery("#nomePaciente").val();
-  jQuery.load("Pacientes1.html", function(){
-    jQuery("#li").html(nome); //li?? Encontrar forma de localizar o texto no HTML em q se deseja colocar o texto
-  });
+class DispPaciente{
+  //Funcões para fazer o display das infos configuradas na lista de pacientes.
+  dispNome= function () { // Nome
+    var nome = jQuery("#nomePaciente").val();
+    jQuery.load("Pacientes1.html", function(){
+      jQuery("#li").html(nome); //li?? Encontrar forma de localizar o texto no HTML em q se deseja colocar o texto
+    });
+  }
+
+  dispIdade = function () { // Idade
+     var nome = jQuery("#idadePaciente").val();
+     jQuery.load("Pacientes1.html", function(){
+       jQuery("#li").html(nome); //li?? Encontrar forma de localizar o texto no HTML em q se deseja colocar o texto
+     });
+  }
+
+  dispTele = function () { // Telefone
+       var nome = jQuery("#telePaciente").val();
+       jQuery.load("Pacientes1.html", function(){
+         jQuery("#li").html(nome); //li?? Encontrar forma de localizar o texto no HTML em q se deseja colocar o texto
+       });
+    }
+
+  dispId = function () { // Prontuário
+     var nome = jQuery("#idPaciente").val();
+     jQuery.load("Pacientes1.html", function(){
+       jQuery("#li").html(nome); //li?? Encontrar forma de localizar o texto no HTML em q se deseja colocar o texto
+     });
+  }
+
+  dispEmail = function () { // E-mail
+     var nome = jQuery("#emailPaciente").val();
+     jQuery.load("Pacientes1.html", function(){
+       jQuery("#li").html(nome); //li?? Encontrar forma de localizar o texto no HTML em q se deseja colocar o texto
+     });
+  }
+
+  dispCausa = function () { // Causa
+     var nome = jQuery("#causaPaciente").val();
+     jQuery.load("Pacientes1.html", function(){
+       jQuery("#li").html(nome); //li?? Encontrar forma de localizar o texto no HTML em q se deseja colocar o texto
+    });
+  }
+
+  dispObs = function () { // Observações
+     var nome = jQuery("#obsPaciente").val();
+     jQuery.load("Pacientes1.html", function(){
+       jQuery("#li").html(nome); //li?? Encontrar forma de localizar o texto no HTML em q se deseja colocar o texto
+     });
+  }
 }
