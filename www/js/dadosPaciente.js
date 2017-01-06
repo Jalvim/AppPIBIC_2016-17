6c73793d1339a5efc3a240dd33aaa1f274a3db39
Jalvim <j.rabello.alvim@outlook.com>
2017-01-06 18:30:44 -0200

    Funcionalidade dos gráficos

--- www/js/dadosPaciente.js
+++ www/js/dadosPaciente.js
@@ -1,5 +1,20 @@
-// FUNCIONALIZAR COM VALORES DE REFERÊNCIA PARA CADA UMA DAS GRANDEZAS MEDIDAS.
-function () {
-    //Escrever aqui o desenvolvimento do código
-}
+var canvas = document.getElementById('myChart');
+var data = {
+  labels: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+  datasets: [
+    [
+      label: "Dados de número de passos",
+      backgroundColor: "rgba:(255, 99, 132, 0.2)",
+      borderColor: "rgba:(255, 99, 132, 1)",
+      borderWidth: 5,
+      hoverBackgroundColor: "rgba(255, 99, 132, 0.4)",
+      hoverBorderColor: "rgba:(255, 99, 132, 1)",
+      data: [10, 20, 30, 40, 50], // AJUSTAR PARA ALTERAÇÃO DINÂMICA COM DB.
+    ]
+  ]
+};
+
+var myBarChart = Chart.Bar(canvas,{
+  data:data
+});
 
