d4008ef4b548cb24c1c6639f87d1ac48589ec474
Matheus Bafutto <matheusbafutto@gmail.com>
2017-03-05 13:55:46 -0300

    mailgun ativado e update de api key/ novo dominio

--- Servidor/mailgunWraper.js
+++ Servidor/mailgunWraper.js
@@ -5,10 +5,10 @@ Este módulo envelopa a funcionalidade do mailgun de envio de emails
 module.exports = function(mail, callback) {
 
 	var api_key = 'key-a2dd99a346672914f2ad5072cd96bc83';
-	var domain = 'sandbox5bf4838706e24e158b9da06c0117db2f.mailgun.org';
+	var domain = 'julianop.com.br';
 	var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
  
- 	mail.from = 'Pulseiras Inteligentes <postmaster@sandbox5bf4838706e24e158b9da06c0117db2f.mailgun.org>';
+ 	mail.from = 'Pulseiras Inteligentes <projetoPulseiras@julianop.com.br>';
 
 	mailgun.messages().send(mail, callback);
 	
