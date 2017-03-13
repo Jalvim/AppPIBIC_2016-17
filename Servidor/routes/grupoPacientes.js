7fda25649a37e30d6a2396d4a3b328897426e5be
Matheus Bafutto <matheusbafutto@gmail.com>
2017-03-12 23:49:43 -0300

    backtrack query busca grupo paciente

--- Servidor/routes/grupoPacientes.js
+++ Servidor/routes/grupoPacientes.js
@@ -280,7 +280,7 @@ router.route('/medicos/')
 	.get(function(req, res){
 		if (req.params.hasOwnProperty('idGrupoPac')) {
 		var query = {
-			sql: `SELECT P.*, GM.idMedico FROM  Paciente P, GrupoPac_Paciente GP, GrupoPac_Medico GM WHERE GP.idPaciente = P.idtable1 AND GP.idGrupoPac = ${connection.escape(req.params.idGrupoPac)} AND GM.idGrupoPac = GP.idGrupoPac`,
+			sql: `SELECT P.* FROM  Paciente P, GrupoPac_Paciente GP WHERE GP.idPaciente	 = P.idtable1 AND GP.idGrupoPac = ${connection.escape(req.params.idGrupoPac)}`,
 			timeout: 10000	
 		}
 		connection.query(query, function(err, rows, fields) {
