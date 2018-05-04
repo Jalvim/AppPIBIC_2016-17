99747d2d83b9239e0e3da276e939f07f7c5f99c7
Bruno Justino <bjpraciano@gmail.com>
2018-05-03 22:30:14 -0300

    Changing cors

--- Servidor/app.js
+++ Servidor/app.js
@@ -89,10 +89,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Tentativa de corrigir CORS para interação com front end
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-	res.header("Access-Control-Allow-Credentials", "false")
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
+	res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+	res.setHeader("Access-Control-Allow-Credentials", true)
 
   next();
 });
