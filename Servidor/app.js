f7058beec51fbd5a8f61ffcf095821c19cca0dd8
Bruno Justino <bjpraciano@gmail.com>
2018-05-03 22:38:28 -0300

    Try again

--- Servidor/app.js
+++ Servidor/app.js
@@ -91,8 +91,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
-	res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-	res.setHeader("Access-Control-Allow-Credentials", true)
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header("Access-Control-Allow-Credentials", "false")
 
   next();
 });
